61e07414dcda81b875313480bd7c2158e2e3dd2c
Pedro Rene Lima Camacho <pedrolc05@gmail.com>
2021-11-12 20:10:12 -0400

    Moved readDate() and longMonth() to sendDataFB.ino file. Better use of headers and files

--- src/sendDataFB.ino
+++ src/sendDataFB.ino
@@ -6,3 +6,17 @@
 void SendFirebase (long sample, String path, float value) {
   Serial.println("Sending the data to Firebase DB");
 }
+
+String readDate () {
+  char buff[21];
+  snprintf_P(buff, sizeof(buff), PSTR("20%02d-%02i-%02iT%02d:%02i:%02iZ"), RTC.getYear(), RTC.getMonth(century), RTC.getDate(), RTC.getHour(h12Flag, pmFlag), RTC.getMinute(), RTC.getSecond());
+  String UTC = String(buff);
+  return UTC;
+}
+
+String longMonth (int month) {
+  const char* months[12] = { "january",  "february", "march",  "abril",  "may",  "june",  "july",
+                             "august",  "october",  "november",  "december" };
+  if (month < 1 || month > 12) return "Month not found!";
+  return months[month-1];
+}
