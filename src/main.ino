7100dbaf6d88e8d7a1e64b0fd7554cfacd07364f
Pedro Rene Lima Camacho <pedrolc05@gmail.com>
2021-11-13 13:22:12 -0400

    add function sendParameter() for all parameters

--- src/main.ino
+++ src/main.ino
@@ -172,24 +172,25 @@ void loop() {
   if (millis() - currentTime > deltaTime) {
     currentTime = millis();
     readWind();
-    float diff, read;
-    read = bme.readTemperature();
-    diff = abs(read - tempOld);
-    if (diff >= 0.21) {
-      tempOld = read;
-      sendFirebase(st++, pathTemp, read);
-      EEPROM.put(EEst, st);
-      EEPROM.commit();
-    }
-
-    read = bme.readHumidity();
-    diff = abs(read - humOld);
-    if (diff >= 1.9) {
-      humOld = read;
-      sendFirebase(sh++, pathHum, read);
-      EEPROM.put(EEsh, sh);
-      EEPROM.commit();
-    }
+    //float diff, read;
+    //read = bme.readTemperature();
+    //diff = abs(read - tempOld);
+    //if (diff >= 0.21) {
+    //  tempOld = read;
+    //  sendFirebase(st++, pathTemp, read);
+    //  EEPROM.put(EEst, st);
+    //  EEPROM.commit();
+    //}
+    sendParameter(bme.readTemperature(), &tempOld, 0.21, EEst, st, pathTemp);
+    //read = bme.readHumidity();
+    //diff = abs(read - humOld);
+    //if (diff >= 1.9) {
+    //  humOld = read;
+    //  sendFirebase(sh++, pathHum, read);
+    //  EEPROM.put(EEsh, sh);
+    //  EEPROM.commit();
+    //}
+    sendParameter(bme.readHumidity(), &humOld, 1.9, EEsh, sh, pathHum);
    // read = bme.readPressure();
    // diff = read - pressOld;
    // if (diff < 0) diff *= -1;
